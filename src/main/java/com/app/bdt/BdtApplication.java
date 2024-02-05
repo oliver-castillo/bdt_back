7a7fdd497ab3df937ac979d119141e0be784ea25
Oliver Castillo <olcastillot@gmail.com>
2024-02-05 14:41:25 -0500

    first commit

---
+++ src/main/java/com/app/bdt/BdtApplication.java
@@ -0,0 +1,13 @@
+package com.app.bdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdtApplication.class, args);
+	}
+
+}
